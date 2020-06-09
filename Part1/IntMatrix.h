8fa430ebdfaebd79768ffef6d4b3025f3f96f7cc
Nativ Maor <nativ.maor@gmail.com>
2020-06-10 01:19:47 +0300

    tried playing with the logic operators, have a few doubts about how it should work.

--- Part1/IntMatrix.h
+++ Part1/IntMatrix.h
@@ -26,18 +26,26 @@ namespace mtm{
         IntMatrix transpose() const;
   
 
-        ///
-
-
+        ///arithmetic operators
         IntMatrix operator+(const IntMatrix& to_add) const;
         IntMatrix operator-() const;
         IntMatrix operator-(const IntMatrix& to_subtract) const;
 
         friend IntMatrix operator+(const IntMatrix& matrix,int to_add);
         friend IntMatrix operator+(int to_add, const IntMatrix& matrix);//does not really need to be a friend
-        friend IntMatrix& operator+=(IntMatrix& matrix,int to_add);
+        friend IntMatrix& operator+=(IntMatrix& matrix,int to_add);// i'm not sure if this function should be symetric also.. currently it seems like it fits to be a member function. 
+
+
+        //logic operators
+        IntMatrix operator==(int compare) const;
+        IntMatrix operator!=(int compare) const;
+        IntMatrix operator<=(int compare) const;
+        IntMatrix operator<(int compare) const;
+        IntMatrix operator>=(int compare) const;
+        IntMatrix operator>(int compare) const;
 
         friend std::ostream& operator<<(std::ostream& os,const IntMatrix& r);
+
         int& operator()(int row,int col);
         int operator()(int row,int col) const;
 
